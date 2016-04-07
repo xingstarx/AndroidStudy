73b7f4ca9d6e08576ecb7ce0f2025cb5b1d672c8
Ztiany <ztiany3@gmail.com>
2016-04-08 01:11:57 +0800

    study fragment opt

--- StudyFragment/src/main/java/com/ztiany/android/operation/FragmentOperationActivity.java
+++ StudyFragment/src/main/java/com/ztiany/android/operation/FragmentOperationActivity.java
@@ -1,9 +1,11 @@
 package com.ztiany.android.operation;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -14,6 +16,7 @@ import com.android.base.ui.BaseActivity;
 import com.android.base.utils.java.Checker;
 import com.ztiany.android.R;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class FragmentOperationActivity extends BaseActivity {
@@ -25,11 +28,10 @@ public class FragmentOperationActivity extends BaseActivity {
 
     private static final int mLayoutId = R.id.act_operation_container_fl;
 
-    private RadioGroup mRadioGroup;
-    private CheckBox mCheckBox;
-
 
     private boolean mAddToStack;
+    private boolean mUseAnim;
+    private boolean mUseAllAnim;
 
     private Fragment mCurrentFragment;
 
@@ -63,9 +65,11 @@ public class FragmentOperationActivity extends BaseActivity {
             mOpt4Fragment = new Opt4Fragment();
         }
 
-        mCheckBox = findView(R.id.act_opt_cb);
-        mRadioGroup = findView(R.id.act_opt_rg);
-        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        CheckBox checkBox = findView(R.id.act_opt_to_stack_cb);
+        CheckBox checkBoxAnim = findView(R.id.act_opt_anim_cb);
+        CheckBox checkBoxAllAnim = findView(R.id.act_opt_all_anim_cb);
+        RadioGroup radioGroup = findView(R.id.act_opt_rg);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.act_opt1_rbtn) {
@@ -79,21 +83,78 @@ public class FragmentOperationActivity extends BaseActivity {
                 }
             }
         });
-        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mAddToStack = isChecked;
             }
+
         });
+
+        checkBoxAnim.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mUseAnim = isChecked;
+                if (mUseAnim) {
+                    mUseAllAnim = false;
+                }
+                Log.d(TAG, "mUseAnim:" + mUseAnim);
+                Log.d(TAG, "mUseAllAnim:" + mUseAllAnim);
+            }
+        });
+
+        checkBoxAllAnim.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mUseAllAnim = isChecked;
+                if (mUseAllAnim) {
+                    mUseAnim = false;
+                }
+                Log.d(TAG, "mUseAnim:" + mUseAnim);
+                Log.d(TAG, "mUseAllAnim:" + mUseAllAnim);
+            }
+        });
+
+
+        String classPath = "android.support.v4.app.FragmentManagerImpl";
+        try {
+            Class<?> aClass = Class.forName(classPath);
+            Field debug = aClass.getDeclaredField("DEBUG");
+            debug.setAccessible(true);
+            Log.d(TAG, "debug:" + debug);
+            try {
+                debug.setBoolean(null, false);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        boolean destroyed = mFragmentManager.isDestroyed();
+        Log.d(TAG, "destroyed:" + destroyed);
     }
 
 
     public void add(View view) {
-        FragmentTransaction add = mFragmentManager.beginTransaction()
-                .add(mLayoutId, mCurrentFragment, mCurrentFragment.getClass().getName());
+        FragmentTransaction add = mFragmentManager.beginTransaction();
+
+        if (mUseAllAnim) {
+            add.setCustomAnimations(R.anim.anim_in, R.anim.anim_out, R.anim.anim_bottom_in, R.anim.anim_bottom_out);
+        }
+        if (mUseAnim) {
+            add.setCustomAnimations(R.anim.anim_in, R.anim.anim_out);
+        }
+
+        add.add(mLayoutId, mCurrentFragment, mCurrentFragment.getClass().getName());
         if (mAddToStack) {
             add.addToBackStack(mCurrentFragment.getClass().getName());
         }
+
+
         add.commit();
     }
 
@@ -132,13 +193,45 @@ public class FragmentOperationActivity extends BaseActivity {
 
 
     public void replace(View view) {
-        FragmentTransaction replace = mFragmentManager.beginTransaction()
-                .replace(mLayoutId, mCurrentFragment, mCurrentFragment.getClass().getName());
+        FragmentTransaction replace = mFragmentManager.beginTransaction();
+
+
+        if (mUseAllAnim) {
+            replace.setCustomAnimations(R.anim.anim_in, R.anim.anim_out, R.anim.anim_bottom_in, R.anim.anim_bottom_out);
+
+        }
+        if (mUseAnim) {
+            replace.setCustomAnimations(R.anim.anim_in, R.anim.anim_out);
+        }
+        replace.replace(mLayoutId, mCurrentFragment, mCurrentFragment.getClass().getName());
         if (mAddToStack) {
             replace.addToBackStack(mCurrentFragment.getClass().getName());
         }
         replace.commit();
     }
+
+    public void popStack(View view) {
+        int backStackEntryCount = mFragmentManager.getBackStackEntryCount();
+        final String[] strings = new String[backStackEntryCount];
+
+        for (int i = 0; i < backStackEntryCount; i++) {
+            FragmentManager.BackStackEntry backStackEntryAt = mFragmentManager.getBackStackEntryAt(i);
+            strings[i] = backStackEntryAt.getName();
+        }
+        new AlertDialog.Builder(this)
+                .setTitle("Select Name")
+                .setSingleChoiceItems(strings, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mFragmentManager.popBackStack(strings[which], FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+
+
+    }
+
 
     public void printInfo(View view) {
 
@@ -154,12 +247,18 @@ public class FragmentOperationActivity extends BaseActivity {
         List<Fragment> fragments = mFragmentManager.getFragments();
         if (!Checker.isEmpty(fragments)) {
             for (Fragment fragment : fragments) {
-                mFragmentManager.beginTransaction()
-                        .remove(fragment)
-                        .commit();
+                if (fragment != null)
+                    mFragmentManager.beginTransaction()
+                            .remove(fragment)
+                            .commit();
             }
         }
     }
 
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "mFragmentManager.isDestroyed():" + mFragmentManager.isDestroyed());
+    }
 }
